1115431d14ebaa22bfd900479a0ddc689f2ae40c
unknown <bb852852bb>
2021-11-16 18:38:53 +0300

    finsh part of login by google

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
 "use strict";
 
-
-
+var user = JSON.parse(localStorage.getItem("result"));
+// console.log(localStorage.getItem("result"));
+console.log(user);
+console.log(user.email);
 
 document.querySelector("textarea").addEventListener("focus", function () {
   document.querySelector(".box_control").classList.remove("d-none");
+});
+document.querySelector(".profile_name").innerHTML = user.displayName;
+document.querySelector(".profile_emails").innerHTML = user.email;
+
+let profile_img = document.querySelectorAll(".profile_img");
+console.log(profile_img);
+profile_img.forEach((element) => {
+  console.log(element);
+  element.setAttribute("src", user.photoURL);
 });
